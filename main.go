167e6cc5445da51a5c0b46c36d4f9e5f3be06251
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-01-08 13:09:50 -0500

    removed samples

--- main.go
+++ main.go
@@ -67,10 +67,3 @@ func main() {
 	// Start server
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
-
-// Request sample
-// {
-// 	"isbn":"4545454",
-// 	"title":"Book Three",
-// 	"author":{"firstname":"Harry","lastname":"White"}
-// }
